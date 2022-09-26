6c89926c5e0a6b4079e37d489a14159ccfa483f6
badman-dev <budmandev@gmail.com>
2022-09-26 19:37:42 -0400

    networked player controllers

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,4 +2,74 @@ using Unity.Netcode;
 using UnityEngine;
 
 public class Player : NetworkBehaviour {
+
+    public NetworkVariable<Vector3> Position = new NetworkVariable<Vector3>();
+    public NetworkVariable<Color> PlayerColor = new NetworkVariable<Color>(Color.red);
+
+    private GameManager _gameMgr;
+
+    public float movementSpeed = .1f;
+
+    public override void OnNetworkSpawn()
+    {
+        if (IsOwner)
+        {
+            _gameMgr = GameObject.Find("GameManager").GetComponent<GameManager>();
+            _gameMgr.RequestNewPlayerColorServerRpc();
+        }
+    }
+
+    Vector3 CalcMovement()
+    {
+        Vector3 moveVect = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        moveVect *= movementSpeed;
+
+        return moveVect;
+    }
+
+    [ServerRpc]
+    void RequestPositionForMovementServerRpc(Vector3 movement)
+    {
+        float planeSize = 5f;
+        Vector3 newPosition = Position.Value + movement;
+        newPosition.x = Mathf.Clamp(newPosition.x, -planeSize, planeSize);
+        newPosition.z = Mathf.Clamp(newPosition.z, -planeSize, planeSize);
+        Position.Value = newPosition;
+    }
+
+
+
+    public void ApplyPlayerColor()
+    {
+        GetComponent<MeshRenderer>().material.color = PlayerColor.Value;
+    }
+
+    public void OnPlayerColorChanged(Color previous, Color current)
+    {
+        ApplyPlayerColor();
+    }
+
+    private void Start()
+    {
+        ApplyPlayerColor();
+        PlayerColor.OnValueChanged += OnPlayerColorChanged;
+    }
+
+    private void FixedUpdate()
+    {
+        if (IsOwner)
+        {
+            Vector3 move = CalcMovement();
+
+            if (move.magnitude > 0)
+            {
+                RequestPositionForMovementServerRpc(move);
+            }
+        }
+        else
+        {
+            transform.position = Position.Value;
+        }
+    }
 }
